72ae5be5d51c87716e2c5fea08d617710796957b
Robin Naumann <constorux@gmail.com>
2021-11-29 12:22:03 +0100

    worked on SettingsPage UI

--- lib/view/my_account/view/my_account_page.dart
+++ lib/view/my_account/view/my_account_page.dart
@@ -1,15 +1,24 @@
 import 'package:fingerfunke_app/common_widgets/image/user_image/user_image.dart';
 import 'package:fingerfunke_app/cubits/authentication_cubit/authentication_cubit.dart';
 import 'package:fingerfunke_app/utils/exceptions.dart';
+import 'package:fingerfunke_app/utils/util_widgets/page_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+const _settingsTitlePadding = EdgeInsets.only(
+  top: 20,
+  left: 15.0,
+  right: 15.0,
+  bottom: 6.0,
+);
 
 class MyAccountPage extends StatelessWidget {
   const MyAccountPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return PageScreen(
       appBar: AppBar(
         title: const Text("Mein Account"),
       ),
@@ -33,6 +42,7 @@ class MyAccountPage extends StatelessWidget {
 }
 
 class TopBar extends StatelessWidget {
+  static const double userImageSize = 80;
   const TopBar({
     Key? key,
   }) : super(key: key);
@@ -43,20 +53,38 @@ class TopBar extends StatelessWidget {
       builder: (context, state) {
         return state.maybeWhen(
             signedIn: (user) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      UserImage(
-                        user.picture,
-                        diameter: 80,
+                      SizedBox.square(
+                        dimension: userImageSize,
+                        child: Stack(
+                          //fit: StackFit.passthrough,
+                          children: [
+                            UserImage(
+                              user.picture,
+                              diameter: userImageSize.round(),
+                            ),
+                            Align(
+                                alignment: Alignment.bottomRight,
+                                child: Container(
+                                    padding: EdgeInsets.all(6),
+                                    decoration: BoxDecoration(
+                                        color: Colors.white.withAlpha(200),
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    child: Icon(Icons.edit, size: 20)))
+                          ],
+                        ),
                       ),
-                      Container(
-                        width: 10,
-                      ),
-                      Text(
-                        user.name,
-                        style: Theme.of(context).textTheme.headline4,
+                      Padding(
+                        padding: const EdgeInsets.only(top: 14),
+                        child: Text(
+                          user.name,
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
                       )
                     ],
                   ),
@@ -74,15 +102,23 @@ class BottomList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SettingsButton(text: "Datenschutz", onPressed: () {}),
-          SettingsButton(text: "Impressum", onPressed: () {}),
-        ],
-      ),
+    return SettingsSection(
+      title: "Informationen",
+      titlePadding: _settingsTitlePadding,
+      tiles: [
+        SettingsTile(
+            title: "Datenschutz",
+            leading: const Icon(Icons.privacy_tip_outlined),
+            onPressed: (context) {}),
+        SettingsTile(
+            title: "Impressum",
+            leading: const Icon(Icons.notes_rounded),
+            onPressed: (context) {}),
+        SettingsTile(
+            title: "Lizenzen",
+            leading: const Icon(Icons.gavel_outlined),
+            onPressed: (context) {}),
+      ],
     );
   }
 }
@@ -94,20 +130,22 @@ class TopList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SettingsButton(text: "SocialMedia", onPressed: () {}),
-          SettingsButton(
-            text: "Account verwalten",
-            onPressed: () {
-              Navigator.of(context).pushNamed("/manageAccount");
-            },
-          ),
-        ],
-      ),
+    return SettingsSection(
+      title: "Account",
+      titlePadding: _settingsTitlePadding,
+      tiles: [
+        SettingsTile(
+            title: "SocialMedia",
+            leading: const Icon(Icons.link_rounded),
+            onPressed: (context) {}),
+        SettingsTile(
+          title: "Account verwalten",
+          leading: const Icon(Icons.account_circle_outlined),
+          onPressed: (context) {
+            Navigator.of(context).pushNamed("/manageAccount");
+          },
+        ),
+      ],
     );
   }
 }
@@ -119,24 +157,23 @@ class DevelopementList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          SettingsButton(
-            text: "Developement utils",
-            onPressed: () {
-              Navigator.of(context).pushNamed("/developementUtils");
-            },
-          ),
-        ],
-      ),
+    return SettingsSection(
+      titlePadding: _settingsTitlePadding,
+      title: "Entwicklung",
+      tiles: [
+        SettingsTile(
+          title: "Developement utils",
+          leading: const Icon(Icons.code_rounded),
+          onPressed: (context) {
+            Navigator.of(context).pushNamed("/developementUtils");
+          },
+        ),
+      ],
     );
   }
 }
 
-class SettingsButton extends StatelessWidget {
+/*class SettingsButton extends StatelessWidget {
   const SettingsButton({
     Key? key,
     required this.text,
@@ -162,4 +199,4 @@ class SettingsButton extends StatelessWidget {
       ),
     );
   }
-}
+}*/
