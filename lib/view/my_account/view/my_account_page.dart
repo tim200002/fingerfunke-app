3ddb2426fdf40e9d2d35697c7516f9c69d961560
Robin Naumann <constorux@gmail.com>
2021-11-29 13:30:30 +0100

    improved settings page layout

--- lib/view/my_account/view/my_account_page.dart
+++ lib/view/my_account/view/my_account_page.dart
@@ -1,17 +1,11 @@
 import 'package:fingerfunke_app/common_widgets/image/user_image/user_image.dart';
 import 'package:fingerfunke_app/cubits/authentication_cubit/authentication_cubit.dart';
+import 'package:fingerfunke_app/utils/dev_tools.dart';
 import 'package:fingerfunke_app/utils/exceptions.dart';
 import 'package:fingerfunke_app/utils/util_widgets/page_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:settings_ui/settings_ui.dart';
-
-const _settingsTitlePadding = EdgeInsets.only(
-  top: 20,
-  left: 15.0,
-  right: 15.0,
-  bottom: 6.0,
-);
 
 class MyAccountPage extends StatelessWidget {
   const MyAccountPage({Key? key}) : super(key: key);
@@ -24,16 +18,16 @@ class MyAccountPage extends StatelessWidget {
       ),
       body: BlocBuilder<AuthenticationCubit, AuthenticationState>(
         builder: (context, state) => state.maybeWhen(
-            signedIn: (_) => Column(
+            signedIn: (_) => ListView(
                   children: const [
-                    TopBar(),
-                    TopList(),
-                    BottomList(),
-                    DevelopementList()
+                    _ProfileSection(),
+                    _AccountSection(),
+                    _InfoSection(),
+                    _DevToolsSection()
                   ],
                 ),
-            signedInAnonymously: () => Column(
-                  children: const [TopList()],
+            signedInAnonymously: () => ListView(
+                  children: const [_AccountSection()],
                 ),
             orElse: () => ErrorWidget(InvalidStateException())),
       ),
@@ -41,9 +35,9 @@ class MyAccountPage extends StatelessWidget {
   }
 }
 
-class TopBar extends StatelessWidget {
-  static const double userImageSize = 80;
-  const TopBar({
+class _ProfileSection extends StatelessWidget {
+  static const double userImageSize = 70;
+  const _ProfileSection({
     Key? key,
   }) : super(key: key);
 
@@ -53,38 +47,54 @@ class TopBar extends StatelessWidget {
       builder: (context, state) {
         return state.maybeWhen(
             signedIn: (user) => Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 15),
-                  child: Column(
+                  padding: const EdgeInsets.symmetric(vertical: 30),
+                  child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      SizedBox.square(
-                        dimension: userImageSize,
-                        child: Stack(
-                          //fit: StackFit.passthrough,
-                          children: [
-                            UserImage(
-                              user.picture,
-                              diameter: userImageSize.round(),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 20),
+                        child: InkWell(
+                          onTap: () => DevTools.showToDoSnackbar(context),
+                          child: SizedBox.square(
+                            dimension: userImageSize,
+                            child: Stack(
+                              //fit: StackFit.passthrough,
+                              children: [
+                                UserImage(
+                                  user.picture,
+                                  diameter: userImageSize.round(),
+                                ),
+                                Align(
+                                    alignment: Alignment.bottomRight,
+                                    child: Container(
+                                        padding: const EdgeInsets.all(6),
+                                        decoration: BoxDecoration(
+                                            color: Colors.white.withAlpha(200),
+                                            borderRadius:
+                                                BorderRadius.circular(20)),
+                                        child:
+                                            const Icon(Icons.edit, size: 20)))
+                              ],
                             ),
-                            Align(
-                                alignment: Alignment.bottomRight,
-                                child: Container(
-                                    padding: EdgeInsets.all(6),
-                                    decoration: BoxDecoration(
-                                        color: Colors.white.withAlpha(200),
-                                        borderRadius:
-                                            BorderRadius.circular(20)),
-                                    child: Icon(Icons.edit, size: 20)))
-                          ],
+                          ),
                         ),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 14),
-                        child: Text(
-                          user.name,
-                          style: Theme.of(context).textTheme.headline4,
-                        ),
+                      Expanded(
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 7),
+                                child: Text(
+                                  user.name,
+                                  style: Theme.of(context).textTheme.headline4,
+                                ),
+                              ),
+                              const Text(
+                                  //ToDo: Show the user's real phone number
+                                  "+49 123 456 7890"),
+                            ]),
                       )
                     ],
                   ),
@@ -95,16 +105,15 @@ class TopBar extends StatelessWidget {
   }
 }
 
-class BottomList extends StatelessWidget {
-  const BottomList({
+class _InfoSection extends StatelessWidget {
+  const _InfoSection({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SettingsSection(
+    return _SettingsSection(
       title: "Informationen",
-      titlePadding: _settingsTitlePadding,
       tiles: [
         SettingsTile(
             title: "Datenschutz",
@@ -123,16 +132,15 @@ class BottomList extends StatelessWidget {
   }
 }
 
-class TopList extends StatelessWidget {
-  const TopList({
+class _AccountSection extends StatelessWidget {
+  const _AccountSection({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SettingsSection(
+    return _SettingsSection(
       title: "Account",
-      titlePadding: _settingsTitlePadding,
       tiles: [
         SettingsTile(
             title: "SocialMedia",
@@ -150,19 +158,18 @@ class TopList extends StatelessWidget {
   }
 }
 
-class DevelopementList extends StatelessWidget {
-  const DevelopementList({
+class _DevToolsSection extends StatelessWidget {
+  const _DevToolsSection({
     Key? key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SettingsSection(
-      titlePadding: _settingsTitlePadding,
+    return _SettingsSection(
       title: "Entwicklung",
       tiles: [
         SettingsTile(
-          title: "Developement utils",
+          title: "Developement tools",
           leading: const Icon(Icons.code_rounded),
           onPressed: (context) {
             Navigator.of(context).pushNamed("/developementUtils");
@@ -173,30 +180,26 @@ class DevelopementList extends StatelessWidget {
   }
 }
 
-/*class SettingsButton extends StatelessWidget {
-  const SettingsButton({
-    Key? key,
-    required this.text,
-    required this.onPressed,
-  }) : super(key: key);
+class _SettingsSection extends StatelessWidget {
+  static const _settingsTitlePadding = EdgeInsets.only(
+    top: 20,
+    left: 15.0,
+    right: 15.0,
+    bottom: 6.0,
+  );
 
-  final String text;
-  final Function onPressed;
+  final List<SettingsTile> tiles;
+  final String title;
+
+  _SettingsSection({required this.title, required this.tiles});
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => onPressed(),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            text,
-            style: Theme.of(context).textTheme.bodyText1,
-          ),
-          const Icon(Icons.arrow_right)
-        ],
-      ),
-    );
+    return Padding(
+        padding: const EdgeInsets.only(bottom: 20),
+        child: Column(children: tiles));
+
+    /*SettingsSection(
+        tiles: tiles, title: title, titlePadding: _settingsTitlePadding);*/
   }
-}*/
+}
