e83983e6b96e6b1eb40b7ae47b0639447242c4e4
Lukas Bormann <44904320+TheBormann@users.noreply.github.com>
2021-12-21 22:44:34 +0100

    feat: added custom logger

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:logger/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
+  Logger.level = Level.verbose;
   runApp(AppInflater());
 }
 
