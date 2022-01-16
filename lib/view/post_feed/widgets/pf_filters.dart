0a317c8f2886b7a1615f394c2db5c10fa9ea6442
Robin Naumann <constorux@gmail.com>
2022-01-17 00:32:11 +0100

    feat: tweaked demo filter UI

--- lib/view/post_feed/widgets/pf_filters.dart
+++ lib/view/post_feed/widgets/pf_filters.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 class PostFeedFilters extends StatelessWidget {
   const PostFeedFilters({Key? key}) : super(key: key);
 
-  Widget _switchSection({required IconData icon, bool selected = false}) {
+  Widget _switchSection(
+      {required IconData icon, bool selected = false, bool right = false}) {
     return Container(
-        color: selected ? Colors.black : Colors.grey.shade200,
-        padding: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          color: selected ? Colors.black : Colors.white,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        padding: const EdgeInsets.all(5),
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 10),
+          padding:
+              EdgeInsets.only(top: 2, bottom: 2, left: right ? 2 : 8, right: 8),
           child: Icon(
             icon,
-            size: 20,
+            size: 18,
             color: selected ? Colors.white : Colors.black,
           ),
         ));
@@ -20,17 +25,17 @@ class PostFeedFilters extends StatelessWidget {
   Widget _eventsGroupsSwitch() {
     return Padding(
       padding: const EdgeInsets.only(right: 15),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(25),
-        child: Container(
-          color: Colors.grey.shade300,
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _switchSection(icon: Icons.mood_rounded, selected: true),
-              _switchSection(icon: Icons.people_rounded),
-            ],
-          ),
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.black),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _switchSection(icon: Icons.mood_rounded, selected: true),
+            _switchSection(icon: Icons.people_rounded, right: true),
+          ],
         ),
       ),
     );
@@ -51,10 +56,15 @@ class PostFeedFilters extends StatelessWidget {
               for (String s in ["sport", "kultur", "theater", "relax"])
                 Padding(
                   padding: const EdgeInsets.only(right: 5),
-                  child: Chip(
-                    label: Text("#$s"),
-                    backgroundColor: Colors.grey.shade200,
-                  ),
+                  child: Container(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+                      child: Text("#$s"),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(30),
+                        border: Border.all(color: Colors.grey.shade300),
+                        color: Colors.white,
+                      )),
                 ),
             ],
           )),
