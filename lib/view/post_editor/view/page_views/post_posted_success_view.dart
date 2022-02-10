686ec20275eb7bfd347d4c87fcd8422bfa9b5c4c
Tim Lindenau <tim.lindenau@outlook.de>
2022-02-10 15:07:46 +0100

    improve post_posted_success_view

--- lib/view/post_editor/view/page_views/post_posted_success_view.dart
+++ lib/view/post_editor/view/page_views/post_posted_success_view.dart
@@ -5,9 +5,26 @@ class PostPostedSuccessView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 1), () {
-      Navigator.of(context).pop();
+    bool hasPopped = false;
+    Future.delayed(const Duration(seconds: 1, milliseconds: 500), () {
+      if (!hasPopped) {
+        Navigator.of(context).pop();
+      }
     });
-    return const Scaffold(body: Center(child: Icon(Icons.check)));
+    return GestureDetector(
+      onTap: () {
+        hasPopped = true;
+        Navigator.of(context).pop();
+      },
+      child: Scaffold(
+        body: Center(
+          child: Icon(
+            Icons.check,
+            color: Theme.of(context).colorScheme.primary,
+            size: 40,
+          ),
+        ),
+      ),
+    );
   }
 }
