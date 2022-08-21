8107dd4f75e8c21e3d2234503dccc338ac1ae9d3
Robin Naumann <constorux@gmail.com>
2022-08-21 15:39:22 +0200

    imp: changed preset value

--- lib/view/home/widgets/filter/cubit/feed_filter_cubit.dart
+++ lib/view/home/widgets/filter/cubit/feed_filter_cubit.dart
@@ -8,7 +8,7 @@ part 'feed_filter_cubit.freezed.dart';
 part 'feed_filter_state.dart';
 
 class FeedFilterCubit extends Cubit<FeedFilterState> {
-  FeedFilterCubit() : super(const FeedFilterState.neutral(10, true, false));
+  FeedFilterCubit() : super(const FeedFilterState.neutral(35, true, false));
 
   void change(FeedFilterState changed) {
     emit(changed);
