5321b35263b1f2e9f104931608829e6f62a2d3ab
Tim Lindenau <tim.lindenau@outlook.de>
2023-05-28 08:32:22 +0200

    add more padding to chat keyboard

--- lib/view/chat/widgets/chat_editor/chat_editor.dart
+++ lib/view/chat/widgets/chat_editor/chat_editor.dart
@@ -29,7 +29,7 @@ class _ChatEditorState extends State<ChatEditor> {
               BlocProvider.of<ChatEditorCubit>(context);
           return SafeArea(
             child: Padding(
-              padding: const EdgeInsets.only(left: 8.0, top: 8.0),
+              padding: const EdgeInsets.only(left: 8.0, top: 8.0, bottom: 8.0),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
